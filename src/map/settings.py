db44a1e84f58d022e9a4990fb4ca2b0bef98d6ce
Benny Daon <bennydaon@gmail.com>
2011-05-14 00:16:04 +0300

    fixing dirs

--- src/map/settings.py
+++ src/map/settings.py
@@ -116,11 +116,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'environmental_data'
+    'map_info'
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
